faee3d9f249664fd8fdb9840fb7cfd3863f41232
AJackTi <dtrong97vn@gmail.com>
2023-03-24 13:08:53 +0700

    Init project

---
+++ rest_errors/rest_errors.go
@@ -0,0 +1,55 @@
+package rest_errors
+
+import (
+	"errors"
+	"net/http"
+)
+
+type RestErr struct {
+	Message string        `json:"message"`
+	Status  int           `json:"status"`
+	Error   string        `json:"error"`
+	Causes  []interface{} `json:"causes"`
+}
+
+func NewError(msg string) error {
+	return errors.New(msg)
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
+
+func NewInternalServerError(message string, err error) *RestErr {
+	result := &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
+	}
+
+	if err != nil {
+		result.Causes = append(result.Causes, err.Error())
+	}
+
+	return result
+}
+
+func NewUnauthorized(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusUnauthorized,
+		Error:   "unauthorized_error",
+	}
+}
---
+++ rest_errors/test_errors_test.go
@@ -0,0 +1,33 @@
+package rest_errors
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewInternalServerError(t *testing.T) {
+	err := NewInternalServerError("this is the message", errors.New("database error"))
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "this is the message", err.Message)
+	assert.EqualValues(t, "internal_server_error", err.Error)
+
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes))
+	assert.EqualValues(t, "database error", err.Causes[0])
+}
+
+func TestNewBadRequestError(t *testing.T) {
+
+}
+
+func TestNewNotFoundError(t *testing.T) {
+
+}
+
+func TestNewError(t *testing.T) {
+
+}
