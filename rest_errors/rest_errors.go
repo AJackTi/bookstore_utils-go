8948693471e36e74811c93b329fbbf5b22a9bf2c
AJackTi <dtrong97vn@gmail.com>
2023-03-24 13:34:01 +0700

    feat: Add new test to rest_errors

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -1,55 +1,70 @@
 package rest_errors
 
 import (
-	"errors"
+	"fmt"
 	"net/http"
 )
 
 type RestErr struct {
-	Message string        `json:"message"`
-	Status  int           `json:"status"`
-	Error   string        `json:"error"`
-	Causes  []interface{} `json:"causes"`
+	Status int           `json:"status"`
+	Title  string        `json:"title"`
+	Detail string        `json:"detail"`
+	Causes []interface{} `json:"causes"`
 }
 
-func NewError(msg string) error {
-	return errors.New(msg)
-}
+// standard error is made based on the error code or a custom error is made based on the message provided.
+func New(code int, err error, message ...string) *RestErr {
+	var restErr *RestErr
 
-func NewBadRequestError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusBadRequest,
-		Error:   "bad_request",
+	if len(message) == 0 {
+		fmt.Printf("Error code:%d was thrown by applicaton...", code)
+		switch code {
+		case 400:
+			restErr = &RestErr{
+				Status: http.StatusBadRequest,
+				Title:  "BAD REQUEST",
+				Detail: "The server cannot or will not process the request due to something that is perceived to be a client error",
+				Causes: []interface{}{err},
+			}
+		case 401:
+			fmt.Println("Unauthorized")
+			restErr = &RestErr{
+				Status: http.StatusUnauthorized,
+				Title:  "UNAUTHORIZED",
+				Detail: "The request has not been applied because it lacks valid authentication credentials for the target resource",
+				Causes: []interface{}{err},
+			}
+		case 404:
+			restErr = &RestErr{
+				Status: http.StatusNotFound,
+				Title:  "NOT FOUND",
+				Detail: "The origin server did not find a current representation for the target resource or is not willing to disclose that one exists.",
+				Causes: []interface{}{err},
+			}
+		case 500:
+			restErr = &RestErr{
+				Status: http.StatusInternalServerError,
+				Title:  "INTERNAL SERVER ERROR",
+				Detail: "The server encountered an unexpected condition that prevented it from fulfilling the request.",
+				Causes: []interface{}{err},
+			}
+		default:
+			restErr = &RestErr{
+				Status: http.StatusNotAcceptable,
+				Title:  "SERVICE UNAVAILABLE",
+				Detail: "The server is currently unable to handle the request due to a temporary overload or scheduled maintenance, which will likely be alleviated after some delay",
+				Causes: []interface{}{err},
+			}
+		}
+	} else {
+		fmt.Println("Custom Bad request error")
+		restErr = &RestErr{
+			Status: code,
+			Title:  "Something went wrong",
+			Detail: message[0],
+			Causes: []interface{}{err},
+		}
 	}
-}
 
-func NewNotFoundError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusNotFound,
-		Error:   "not_found",
-	}
-}
-
-func NewInternalServerError(message string, err error) *RestErr {
-	result := &RestErr{
-		Message: message,
-		Status:  http.StatusInternalServerError,
-		Error:   "internal_server_error",
-	}
-
-	if err != nil {
-		result.Causes = append(result.Causes, err.Error())
-	}
-
-	return result
-}
-
-func NewUnauthorized(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusUnauthorized,
-		Error:   "unauthorized_error",
-	}
+	return restErr
 }
--- rest_errors/test_errors_test.go
+++ rest_errors/test_errors_test.go
@@ -9,25 +9,49 @@ import (
 )
 
 func TestNewInternalServerError(t *testing.T) {
-	err := NewInternalServerError("this is the message", errors.New("database error"))
+	err := New(http.StatusInternalServerError, errors.New("database error"))
 	assert.NotNil(t, err)
 	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
-	assert.EqualValues(t, "this is the message", err.Message)
-	assert.EqualValues(t, "internal_server_error", err.Error)
+	assert.EqualValues(t, "The server encountered an unexpected condition that prevented it from fulfilling the request.", err.Detail)
+	assert.EqualValues(t, "INTERNAL SERVER ERROR", err.Title)
 
 	assert.NotNil(t, err.Causes)
 	assert.EqualValues(t, 1, len(err.Causes))
-	assert.EqualValues(t, "database error", err.Causes[0])
+	assert.EqualValues(t, "database error", err.Causes[0].(error).Error())
 }
 
 func TestNewBadRequestError(t *testing.T) {
+	err := New(http.StatusBadRequest, errors.New("invalid argument"))
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusBadRequest, err.Status)
+	assert.EqualValues(t, "The server cannot or will not process the request due to something that is perceived to be a client error", err.Detail)
+	assert.EqualValues(t, "BAD REQUEST", err.Title)
 
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes))
+	assert.EqualValues(t, "invalid argument", err.Causes[0].(error).Error())
 }
 
 func TestNewNotFoundError(t *testing.T) {
+	err := New(http.StatusNotFound, errors.New("not found"))
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.Status)
+	assert.EqualValues(t, "The origin server did not find a current representation for the target resource or is not willing to disclose that one exists.", err.Detail)
+	assert.EqualValues(t, "NOT FOUND", err.Title)
 
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes))
+	assert.EqualValues(t, "not found", err.Causes[0].(error).Error())
 }
 
 func TestNewError(t *testing.T) {
+	err := New(http.StatusBadRequest, errors.New("invalid database"), "Something went wrong")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusBadRequest, err.Status)
+	assert.EqualValues(t, "Something went wrong", err.Detail)
+	assert.EqualValues(t, "Something went wrong", err.Title)
 
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes))
+	assert.EqualValues(t, "invalid database", err.Causes[0].(error).Error())
 }
