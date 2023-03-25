84608c0d7c4d971c38f4387640a0e6c61e2ffdce
AJackTi <dtrong97vn@gmail.com>
2023-03-25 11:00:32 +0700

    feat: update logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,13 +1,19 @@
 package logger
 
 import (
+	"fmt"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 var (
-	log *zap.Logger
+	log logger
 )
+
+type logger struct {
+	log *zap.Logger
+}
 
 func init() {
 	logConfig := zap.Config{
@@ -25,18 +31,30 @@ func init() {
 	}
 
 	var err error
-	if log, err = logConfig.Build(); err != nil {
+	if log.log, err = logConfig.Build(); err != nil {
 		panic(err)
 	}
 }
 
+func (l logger) Printf(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+	} else {
+		Info(fmt.Sprintf(format, v...))
+	}
+}
+
+func GetLogger() logger {
+	return log
+}
+
 func Info(msg string, tags ...zap.Field) {
-	log.Info(msg, tags...)
-	log.Sync()
+	log.log.Info(msg, tags...)
+	log.log.Sync()
 }
 
 func Error(msg string, err error, tags ...zap.Field) {
 	tags = append(tags, zap.NamedError("error", err))
-	log.Error(msg, tags...)
-	log.Sync()
+	log.log.Error(msg, tags...)
+	log.log.Sync()
 }
